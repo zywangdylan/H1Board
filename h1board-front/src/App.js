9e2efac8209f3f5a015cebf05cec57c4d057bea0
zhenyuan wang <zywangdylan@gmail.com>
2023-04-01 20:39:54 -0400

    init with hw2 code

--- h1board-front/src/App.js
+++ h1board-front/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { indigo, amber } from '@mui/material/colors'
+import { createTheme } from "@mui/material/styles";
 
-function App() {
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import AlbumsPage from './pages/AlbumsPage';
+import SongsPage from './pages/SongsPage';
+import AlbumInfoPage from './pages/AlbumInfoPage'
+
+// createTheme enables you to customize the look and feel of your app past the default
+// in this case, we only change the color scheme
+export const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: amber,
+  },
+});
+
+// App is the root component of our application and as children contain all our pages
+// We use React Router's BrowserRouter and Routes components to define the pages for
+// our application, with each Route component representing a page and the common
+// NavBar component allowing us to navigate between pages (with hyperlinks)
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/albums" element={<AlbumsPage />} />
+          <Route path="/albums/:album_id" element={<AlbumInfoPage />} />
+          <Route path="/songs" element={<SongsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
-export default App;
