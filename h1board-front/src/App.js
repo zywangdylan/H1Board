a4f2d60f1d10401a02dc9c3b7895fe52b09d52fc
zhenyuan wang <zywangdylan@gmail.com>
2023-04-30 00:10:40 -0400

    added continue with google button on login and signup page

--- h1board-front/src/App.js
+++ h1board-front/src/App.js
@@ -3,12 +3,19 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { blue, yellow } from '@mui/material/colors'
 import { createTheme } from "@mui/material/styles";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import CompaniesPage from './pages/CompaniesPage';
 import CompanyPage from './pages/CompanyPage';
+<<<<<<< Updated upstream
 import AlbumInfoPage from './pages/AlbumInfoPage'
+=======
+import LogoutComponent from './components/Logout/Logout';
+import { gridColumnGroupingSelector } from "@mui/x-data-grid";
+>>>>>>> Stashed changes
 
 // createTheme enables you to customize the look and feel of your app past the default
 // in this case, we only change the color scheme
@@ -21,6 +28,19 @@ export const theme = createTheme({
 
 export default function App() {
   const location = useLocation();
+
+  const handleCallbackres = (res) => {
+    const userObject = jwt_decode(res.credential)
+    console.log(userObject)
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '659539889110-06dnqllqgguev2mb3gfgl6mhq30k2qiq.apps.googleusercontent.com',
+      callback: handleCallbackres,
+    });
+  }, [])
 
   return (
     <ThemeProvider theme={theme}>
